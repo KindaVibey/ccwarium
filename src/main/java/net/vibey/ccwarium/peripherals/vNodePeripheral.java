a602e74f499a32e7aa46b43466e9116035c13ae8
KindaVibey <joshuaspirek@gmail.com>
2025-09-26 16:35:13 -0400

    fixed wrong output

--- src/main/java/net/vibey/ccwarium/peripherals/vNodePeripheral.java
+++ src/main/java/net/vibey/ccwarium/peripherals/vNodePeripheral.java
@@ -31,28 +31,28 @@ public class vNodePeripheral implements IPeripheral {
     @LuaFunction
     public void setNodePitch(double pitch) throws LuaException {
         if (pitch > 1 || pitch < -1) throw new LuaException("Control surface values can only match -1,0,1");
-        node.getPersistentData().putDouble("Pitch",pitch/1);
+        node.getPersistentData().putDouble("Pitch",pitch);
         BlockState state = node.getLevel().getBlockState(node.getBlockPos());
         node.getLevel().sendBlockUpdated(node.getBlockPos(),state,state,3);
     }
     @LuaFunction
     public void setNodeYaw(double yaw) throws LuaException {
         if (yaw > 1 || yaw < -1) throw new LuaException("Control surface values can only match -1,0,1");
-        node.getPersistentData().putDouble("Yaw",yaw/1);
+        node.getPersistentData().putDouble("Yaw",yaw);
         BlockState state = node.getLevel().getBlockState(node.getBlockPos());
         node.getLevel().sendBlockUpdated(node.getBlockPos(),state,state,3);
     }
     @LuaFunction
     public void setNodeRoll(double roll) throws LuaException {
         if (roll > 1 || roll < -1) throw new LuaException("Control surface values can only match -1,0,1");
-        node.getPersistentData().putDouble("Roll", roll /1);
+        node.getPersistentData().putDouble("Roll", roll);
         BlockState state = node.getLevel().getBlockState(node.getBlockPos());
         node.getLevel().sendBlockUpdated(node.getBlockPos(),state,state,3);
     }
     @LuaFunction
     public void setNodeThrottle(double throttle) throws LuaException {
         if (throttle > 10 || throttle < -1) throw new LuaException("Throttle values can only be -1-10");
-        node.getPersistentData().putDouble("Throttle",throttle/10);
+        node.getPersistentData().putDouble("Throttle",throttle);
         BlockState state = node.getLevel().getBlockState(node.getBlockPos());
         node.getLevel().sendBlockUpdated(node.getBlockPos(),state,state,3);
     }
@@ -70,7 +70,7 @@ public class vNodePeripheral implements IPeripheral {
         double trig;
         if (trigger) trig = 1;
         else trig = 0;
-        node.getPersistentData().putDouble("Trigger"+channel+".0", trig/1);
+        node.getPersistentData().putDouble("Trigger"+channel+".0", trig);
         BlockState state = node.getLevel().getBlockState(node.getBlockPos());
         node.getLevel().sendBlockUpdated(node.getBlockPos(), state, state, 3);
     }
